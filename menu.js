4b2c8189e1d6309519bc1b3ce6781703496beb40
Repingjiang <lyp141125@gmail.com>
2023-10-18 03:09:22 -0500

    update the about page, and show menu correctly

--- menu.js
+++ menu.js
@@ -3,16 +3,15 @@ fetch("menu.json")
     .then(dishMenu => loadDishes(dishMenu));
 
 function loadDishes(dishMenu) {
-    // Variables for the cards:
     let imgDishes = [];
     let txtDishes = [];
-    let descDishes = []; // New variable for descriptions
-    for (let i = 1; i <= 9; i++) {
+    let descDishes = [];
+    
+    for (let i = 1; i <= 10; i++) {
         imgDishes.push(document.getElementById(`imgDish${i}`));
         txtDishes.push(document.getElementById(`txtDish${i}`));
-        descDishes.push(document.getElementById(`descDish${i}`)); // New line
+        descDishes.push(document.getElementById(`descDish${i}`));
 
-        // Toggle buttons logic:
         let toggleButton = document.getElementById(`toggleCardButton${i}`);
         let card = document.getElementById(`card${i}`);
         let collapsableCard = new bootstrap.Collapse(card, { toggle: false });
@@ -24,8 +23,9 @@ function loadDishes(dishMenu) {
     for (let i = 0; i < dishMenu.dishes.length; i++) {
         let dishName = dishMenu.dishes[i].dishName;
         let dishPrice = dishMenu.dishes[i].dishPrice;
-        let dishDescription = dishMenu.dishes[i].description; // New line
+        let dishDescription = dishMenu.dishes[i].description;
         let url = dishMenu.dishes[i].url;
+        let dishId = dishMenu.dishes[i].dishId;  // Declare and assign the dishId
         
         let imgDish = document.createElement("div");
         imgDish.innerHTML = `<img src=${url} class="card-img-top" alt="${dishName}"></img>`;
@@ -33,47 +33,64 @@ function loadDishes(dishMenu) {
         let txtDish = document.createElement("p");
         txtDish.innerHTML = `<p class="card-text"> <strong>${dishName}</strong>, $${dishPrice}</p>`;
         
-        let descDish = document.createElement("p"); // New section
-        descDish.innerHTML = `<p class="card-description">${dishDescription}</p>`; // New section
+        let descDish = document.createElement("p");
+        descDish.innerHTML = `<p class="card-description">${dishDescription}</p>`;
 
-        switch (dishName) {
-            case "Chicken Caesar Salad":
+
+        switch (dishId) {
+            case "1234": 
                 imgDishes[0].appendChild(imgDish);
                 txtDishes[0].appendChild(txtDish);
+                descDishes[0].appendChild(descDish);
                 break;
-            case "Vegetable Stir Fry":
+            case "1235": 
                 imgDishes[1].appendChild(imgDish);
                 txtDishes[1].appendChild(txtDish);
+                descDishes[1].appendChild(descDish);
                 break;
-            case "Spaghetti Carbonara":
+            case "1236": 
                 imgDishes[2].appendChild(imgDish);
                 txtDishes[2].appendChild(txtDish);
+                descDishes[2].appendChild(descDish);
                 break;
-            case "Grilled Salmon":
+            case "1237": 
                 imgDishes[3].appendChild(imgDish);
                 txtDishes[3].appendChild(txtDish);
+                descDishes[3].appendChild(descDish);
                 break;
-            case "Beef Steak":
+            case "1238": 
                 imgDishes[4].appendChild(imgDish);
                 txtDishes[4].appendChild(txtDish);
+                descDishes[4].appendChild(descDish);
                 break;
-            case "Veggie Burger":
+            case "1239": 
                 imgDishes[5].appendChild(imgDish);
                 txtDishes[5].appendChild(txtDish);
+                descDishes[5].appendChild(descDish);
                 break;
-            case "Shrimp Tacos":
+            case "1240": 
                 imgDishes[6].appendChild(imgDish);
                 txtDishes[6].appendChild(txtDish);
+                descDishes[6].appendChild(descDish);
                 break;
-            case "Lamb Chops with Mint Sauce":
+            case "1241": 
                 imgDishes[7].appendChild(imgDish);
                 txtDishes[7].appendChild(txtDish);
+                descDishes[7].appendChild(descDish);
                 break;
-            case "Eggplant Parmesan":
+            case "1242": 
                 imgDishes[8].appendChild(imgDish);
                 txtDishes[8].appendChild(txtDish);
+                descDishes[8].appendChild(descDish);
+                break;
+            case "1243": 
+                imgDishes[9].appendChild(imgDish);
+                txtDishes[9].appendChild(txtDish);
+                descDishes[9].appendChild(descDish);
                 break;
         }
+        
+        
 
         // switch (dishName) {
         //     case "Chicken Caesar Salad":
