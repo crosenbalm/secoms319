03a1e6db971669929b1477c6086a73c7ace84091
crosenbalm <crosenbalm02@gmail.com>
2023-10-27 13:08:55 -0500

    button

--- Assignment 02/assignment_02/src/App.js
+++ Assignment 02/assignment_02/src/App.js
@@ -9,13 +9,9 @@ export function App() {
     return (
         <>
         <div class="col">
-          <button id="toggleCardButton6" type="button" class="btn btn-primary mb-2">Toggle Card</button>
-          <div id="card1" class="card collapse show shadow-sm">
-            <div id="imgDish6"></div>
-            <div class="card-body">
-              <p id="txtDish6" class="card-text"></p>
-              <p id="descDish6" class="card-description"></p>
-            </div>
+          <button>Toggle Card</button>
+          <div>
+              <p>Testing</p>
           </div>
         </div>
         </>
