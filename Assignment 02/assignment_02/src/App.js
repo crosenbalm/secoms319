13b83d78663b873f172884242e1b8b5b28fadbda
crosenbalm <crosenbalm02@gmail.com>
2023-10-27 13:04:37 -0500

    files

--- Assignment 02/assignment_02/src/App.js
+++ Assignment 02/assignment_02/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import Cart from "./Cart";
+import Confirmation from "./Confirmation";
+
+export function App() {
+    return (
+        <>
+        <div class="col">
+          <button id="toggleCardButton6" type="button" class="btn btn-primary mb-2">Toggle Card</button>
+          <div id="card1" class="card collapse show shadow-sm">
+            <div id="imgDish6"></div>
+            <div class="card-body">
+              <p id="txtDish6" class="card-text"></p>
+              <p id="descDish6" class="card-description"></p>
+            </div>
+          </div>
+        </div>
+        </>
+    )
 }
-
-export default App;
