6bc745505317239de60329dc20ea85d86c2b2845
crosenbalm <crosenbalm02@gmail.com>
2023-11-05 11:23:53 -0600

    base shop

--- Assignment 02/assignment_02/src/index.js
+++ Assignment 02/assignment_02/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './App';
+import Shop from "./Shopping.js";
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <Shop />
     </React.StrictMode>
 );
